d03ebd1657340f2dcd2cc01567a81e4917bd0638
robinroy03 <robinroy.work@gmail.com>
2025-10-21 19:47:11 +0000

    assignment 1

---
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Robin Roy and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Flight Passenger", {
+// 	refresh(frm) {
+
+// 	},
+// });
